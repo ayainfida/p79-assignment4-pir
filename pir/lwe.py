2f044904028a847bacf9c1ea0c97d15f755b4969
Ayain Fida <ayain.fida@outlook.com>
2026-03-14 10:54:13 +0000

    lwe dims generalized

--- pir/lwe.py
+++ pir/lwe.py
@@ -13,39 +13,44 @@ These methods will be used by the PIR scheme.
 """
 class LWEMethods:
     @staticmethod
-    def sample_error_vector(N: int, q: int, B: int = 3) -> np.ndarray[RingElement]:
+    def sample_error_vector(N: int, q: int, dtype: type, B: int = 3) -> np.ndarray[RingElement]:
         """
         Sample small error vector from the interval [-B, B].
 
         Args:
             N: length of vector
             q: modulo integer for the ring
+            dtype: data type for the vector, either np.uint8 or bool
             B: bound for error
 
         Returns:
             error vector of length N of RingElements
         """
-        error = np.random.randint(-B, B + 1, size=N)
+        n_bits = 8 if dtype == np.uint8 else 1
+        error = np.random.randint(-B, B + 1, size=N*n_bits)
 
-        return RingElement.get_ring_vector(error, q)
+        return RingElement.get_ring_vector(error, q).reshape(n_bits, N)
 
     @staticmethod
-    def sample_secret_vector(N: int, q: int) -> np.ndarray[RingElement]:
+    def sample_secret_vector(N: int, q: int, dtype: type) -> np.ndarray[RingElement]:
         """
         Sample secret vector from over the interval {-q, q}.
 
         Args:
             N: length of vector
             q: modulo integer for the ring
+            dtype: data type for the vector, either np.uint8 or bool
 
         Returns:
             secret vector of length N of RingElements
         """
-        secret = np.random.randint(-q, q, size=N)
-        return RingElement.get_ring_vector(secret, q)
+        n_bits = 8 if dtype == np.uint8 else 1
+        secret = np.random.randint(-q, q, size=N*n_bits)
+
+        return RingElement.get_ring_vector(secret, q).reshape(n_bits, N)
     
     @staticmethod
-    def generate_matrix_A(N: int, n: int, q: int, seed: int) -> np.ndarray[RingElement]:
+    def generate_matrix_A(N: int, n: int, q: int, dtype: type, seed: int) -> np.ndarray[RingElement]:
         """
         Generate a random matrix A of size M x N with entries uniformly sampled from the ring given a fixed seed.
 
@@ -53,20 +58,20 @@ class LWEMethods:
             N: number of rows
             n: number of columns
             q: modulo integer for the ring
+            dtype: data type for the matrix, either np.uint8 or bool
             seed: random seed
 
         Returns:
             A random matrix of size N x n of RingElements
         """
         np.random.seed(seed)
-        A = np.random.randint(-q, q, size=(N, n))
-        return RingElement.get_ring_vector(A.flatten(), q).reshape(N, n)
+        n_bits = 8 if dtype == np.uint8 else 1
+        
+        return RingElement.get_ring_vector(A.flatten(), q).reshape(n_bits, N, n)
     
-
-
 if __name__ == "__main__":
     # Example usage of the LWE methods
-    error_vector = LWEMethods.sample_error_vector(N=10, q =32)
+    error_vector = LWEMethods.sample_error_vector(N=10, q =32, dtype=np.uint8)
     print("Sampled error vector:", error_vector)
     # print("Type of error vector elements:", type(error_vector[0]))
     # secret_vector = LWEMethods.sample_secret_vector(N=10)
