03474c38c5fea96b5c7c2e9c856b82b3e4971996
Ayain Fida <ayain.fida@outlook.com>
2026-03-14 11:45:01 +0000

    encodings finalized for all schemes

--- pir/encoding.py
+++ pir/encoding.py
@@ -24,24 +24,25 @@ def encode_std_pir(A: np.ndarray[RingElement], c: np.ndarray[RingElement], lengt
     """
     # Encode A and c as bytes
     A_bytes = b''.join(encode_little_endian(a.value, length) for a in A.flatten())
-    c_bytes = b''.join(encode_little_endian(c_i.value, length) for c_i in c)
+    c_bytes = b''.join(encode_little_endian(c_i.value, length) for c_i in c.flatten())
 
     return A_bytes + c_bytes  
 
-def decode_std_pir(data: bytes, N: int, n: int, q: int, length: int = 32) -> tuple[np.ndarray[RingElement], np.ndarray[RingElement]]:
+def decode_std_pir(data: bytes, N: int, n: int, q: int, dtype: type, length: int = 32) -> tuple[np.ndarray[RingElement], np.ndarray[RingElement]]:
     """
     Decode the standard PIR query from bytes:
     A | c
     """
     # We first calculate the expected length of A and c in bytes
-    A_length = N * n * length
-    c_length = N * length    
+    n_bits = 8 if dtype == np.uint8 else 1
+    A_length = N * n * length * n_bits
+    c_length = N * length  * n_bits 
 
-    assert len(data) == A_length + c_length, "Invalid data length for decoding. Expected: {} and got: {}".format(A_length + c_length, len(data))
+    assert len(data) == A_length + c_length, f"Invalid data length for decoding. Expected: {A_length + c_length} and got: {len(data)}"
 
     # Decode A and c from bytes
-    A = RingElement.get_ring_vector(np.array([decode_little_endian(data[i:i+length]) for i in range(0, A_length, length)], dtype=object), q).reshape(N, n)
-    c = RingElement.get_ring_vector(np.array([decode_little_endian(data[A_length + i:A_length + i + length]) for i in range(0, c_length, length)], dtype=object), q)
+    A = RingElement.get_ring_vector(np.array([decode_little_endian(data[i:i+length]) for i in range(0, A_length, length)], dtype=object), q).reshape(n_bits, N, n)
+    c = RingElement.get_ring_vector(np.array([decode_little_endian(data[A_length + i:A_length + i + length]) for i in range(0, c_length, length)], dtype=object), q).reshape(n_bits, N)
 
     return A, c
 
@@ -49,15 +50,16 @@ def encode_opt_pir(c: np.ndarray[RingElement], length: int = 32) -> bytes:
     """
     Encode the vector c into bytes
     """
-    return b''.join(encode_little_endian(c_i.value, length) for c_i in c)
+    return b''.join(encode_little_endian(c_i.value, length) for c_i in c.flatten())
 
-def decode_opt_pir(data: bytes, N: int, q: int, length: int = 32) -> np.ndarray[RingElement]:
+def decode_opt_pir(data: bytes, N: int, q: int, dtype: type, length: int = 32) -> np.ndarray[RingElement]:
     """
     Decode the vector c from bytes
     """
-    assert len(data) == N * length, "Invalid data length for decoding. Expected: {} and got: {}".format(N * length, len(data))
+    n_bits = 8 if dtype == np.uint8 else 1
+    assert len(data) == N * length * n_bits, f"Invalid data length for decoding. Expected: {N * length * n_bits} and got: {len(data)}"
 
-    c = RingElement.get_ring_vector(np.array([decode_little_endian(data[i:i+length]) for i in range(0, len(data), length)], dtype=object), q)
+    c = RingElement.get_ring_vector(np.array([decode_little_endian(data[i:i+length]) for i in range(0, len(data), length)], dtype=object), q).reshape(n_bits, N)
 
     return c
 
@@ -70,15 +72,17 @@ def encode_hint(seed: int, A: np.ndarray[RingElement], length: int = 32) -> byte
 
     return seed_bytes + A_bytes
 
-def decode_hint(data: bytes, N: int, n: int, q: int, length: int = 32) -> tuple[int, np.ndarray[RingElement]]:
+def decode_hint(data: bytes, N: int, n: int, q: int, dtype: type, length: int = 32) -> tuple[int, np.ndarray[RingElement]]:
     """
     Decode the hint from bytes.
     """
-    A_length = N * n * length
+    n_bits = 8 if dtype == np.uint8 else 1
+    A_length = N * n * length * n_bits
 
-    assert len(data) == length + A_length, "Invalid data length for decoding. Expected: {} and got: {}".format(length + A_length, len(data))
-
+    assert len(data) == length + A_length, f"Invalid data length for decoding. Expected: {length + A_length} and got: {len(data)}"
     seed = decode_little_endian(data[:length])
-    A = RingElement.get_ring_vector(np.array([decode_little_endian(data[length + i:length + i + length]) for i in range(0, A_length, length)], dtype=object), q).reshape(N, n)
+    A = RingElement.get_ring_vector(np.array([decode_little_endian(data[length + i:length + i + length]) for i in range(0, A_length, length)], dtype=object), q)
+    
+    A = A.reshape(n_bits, N, n)
 
     return seed, A
